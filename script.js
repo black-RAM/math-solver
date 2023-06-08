dfa0cefdcf75c5029110d15baae2868fbe31dad7
black-RAM <pplanes311@gmail.com>
2023-06-08 21:17:18 +0300

    added code to evaluage expression when a seccond operator is clicked

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ function operate(input) {
   const x = parseFloat(input.match(/(\d+)/)); // match first number
   const o = input.match(/[+x÷^-]/)[0]; // match operator
   const y = parseFloat(input.match(/(?<=[+\-x÷^])\d+/)); // match number after operator
-
-  // Define the mapping of operators to functions
+  // mapping of operators to functions
   const operators = {
     '+': (a, b) => a + b,
     '-': (a, b) => a - b,
@@ -11,44 +10,49 @@ function operate(input) {
     '÷': (a, b) => a / b,
     '^': (a, b) => Math.pow(a, b)
   };
-
   const operatorFunc = operators[o]; // access function for operator
-
   let ans = operatorFunc(+x, +y);// pass numbers as function parameters
-
   return ans; // return result
 }
 
-// populate display
+// DOM reference to calc displays and clickable buttons
 const displayA = document.getElementById('main-display');
 const displayB = document.getElementById('up-display');
-
 const toDisplay = [
   ...document.getElementsByClassName('num'),
   ...document.getElementsByClassName('o')
 ];
 
-let onDisplay = [];
+let onDisplay = "";
+let opNum = 0;
 
 toDisplay.forEach((button) => {
 
   button.addEventListener('click', () => {
     let char = button.innerText;
-    onDisplay.push(char);
-    
-    let isOp = /[+\-x÷=]/.test(char);
+    onDisplay+=char;
 
-    if(isOp) {
-      displayB.innerText = onDisplay.join('');
+    if(/[+x÷^=-]/.test(char)) {
+      displayB.innerText = onDisplay;
       displayA.innerText = "";
+      opNum++;
     } else {
       displayA.innerText += char;
+    };
+    
+    // evaluate
+    if(opNum > 1) {
+      let result = operate(onDisplay);
+      displayA.innerText = result;
+      if(char === "=") {
+        onDisplay = [result];
+        opNum = 0;
+      } else {
+        onDisplay = [result, char].join("");
+        opNum = 1;
+        displayB.innerText = onDisplay;
+        displayA.innerText = "";
+      }
     }
   });
 });
-
-// evaluate when = clicked
-document.getElementById('equals').addEventListener('click', () => {
-  operate(onDisplay.join(''))
-  onDisplay = [];
-})
