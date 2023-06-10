0a5d008b72c65afa933a1ad0e6192b8017e40fe5
black-RAM <pplanes311@gmail.com>
2023-06-10 18:24:18 +0300

    keyboard functionality

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ function operate(input) {
   const operators = {
     '+': (a, b) => a + b,
     '-': (a, b) => a - b,
-    'x': (a, b) => a * b,
-    '÷': (a, b) => a / b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => a / b,
     '^': (a, b) => Math.pow(a, b)
   };
+  // Add additional keys for certain operators
+  operators['÷'] = operators['/']; 
+  operators['x'] = operators['*'];
+
   const operatorFunc = operators[o]; // access function for operator
 
   let ans = operatorFunc(parseFloat(x), parseFloat(y));// pass numbers as function parameters
@@ -52,7 +56,7 @@ toDisplay.forEach((button) => {
         return; // Skip appending the decimal point
       }
     }
-    
+
     if(/[+x÷^=-]/.test(char)) { // if character is an operator
       currentDisplay = displayB;
       onDisplay+=` ${char} `;
@@ -104,3 +108,61 @@ clears[1].addEventListener('click', () => {
   displayA.innerText = '';
   displayB.innerText ='';
 }); // clear all
+
+// Keyboard functionality
+document.addEventListener('keydown', (event) => {
+  const key = event.key;
+  handleKeyPress(key);
+});
+
+function handleKeyPress(key) {
+  // Check if the key is a number
+  if (/^\d$/.test(key)) {
+    onDisplay += key;
+    currentDisplay = displayA;
+    currentDisplay.innerText += key;
+  }
+
+  // if key is an operator
+  if(/^[+x÷^=-]$/.test(key)) {
+    onDisplay+=` ${key} `;
+    currentDisplay = displayB;
+    currentDisplay.innerText = onDisplay;
+    displayA.innerText = "";
+    opNum++;
+  };
+
+  // Check if the key is the decimal point (.)
+  if (key === '.') {
+    // prevent multiple decimals
+    const lastOperand = onDisplay.split(' ').pop();
+    if (lastOperand.includes('.')) {
+      return;
+    }
+    onDisplay += key;
+    currentDisplay = displayA;
+    currentDisplay.innerText = onDisplay;
+  }
+
+  // Check if the key is the equals sign (=)
+  if (key === '=') {
+    let result = operate(onDisplay);
+    onDisplay = result;
+    opNum = 0;
+    displayA.innerText = onDisplay;
+  }
+
+  // Check if the key is the backspace key
+  if (key === 'Backspace') {
+    onDisplay = onDisplay.slice(0, -1);
+    currentDisplay.innerText = onDisplay;
+  }
+
+  // Check if the key is the Escape key (clear all)
+  if (key === 'Escape') {
+    onDisplay = '';
+    displayA.innerText = '';
+    displayB.innerText = '';
+    opNum = 0;
+  }
+}
